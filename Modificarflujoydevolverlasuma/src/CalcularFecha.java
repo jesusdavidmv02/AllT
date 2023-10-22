d5e6c24b772bf7dd94e04f577e3471c496e14aa0
Jesus Mendoza <jesusdavidmv02@gmail.com>
2023-10-22 00:06:15 -0500

    nuevo archivios nuevos cambios ::::

--- Modificarflujoydevolverlasuma/src/CalcularFecha.java
+++ Modificarflujoydevolverlasuma/src/CalcularFecha.java
@@ -1,6 +1,10 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Para esta tarea se pide ingresar una fecha de nacimiento en formato string, convertirla a una
@@ -13,10 +17,16 @@ public class CalcularFecha {
 
     public static void main(String[] args) {
 
-        System.out.println("digete la tu años de nacimento ");
+        Scanner sc = new Scanner(System.in);
 
-        Date hoy  = new Date();
+        System.out.println("Ingrese un fecha de nacimiento con formato yyyy-MM-dd:");
+        String fechaStr = sc.nextLine();
+
+        LocalDate fechaNacimiento = LocalDate.parse(fechaStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        LocalDate actual = LocalDate.now();
+
+        Period  periodo = Period.between(fechaNacimiento, actual);
+        System.out.printf("Tu edad es: %s años, %s meses y %s días", periodo.getYears(), periodo.getMonths(), periodo.getDays());
 
     }
-
 }
