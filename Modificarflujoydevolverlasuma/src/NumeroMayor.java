c82c9239a8191eb1b2aa076c15cda1153ef51124
Jesus Mendoza <jesusdavidmv02@gmail.com>
2023-10-21 21:47:34 -0500

    nuevo archivios

---
+++ Modificarflujoydevolverlasuma/src/NumeroMayor.java
@@ -0,0 +1,17 @@
+/***
+
+ El siguiente ejercicio es obtener el numero mayor de un arreglo pero utilizando programación funcional,
+ implementando la expresión lambda Function<T,R> (que viene predefinida) con el api stream y operador reduce.
+ Preguntas de esta tarea
+ Publica acá tu código fuente de la tarea
+ *
+ */
+
+public class NumeroMayor {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
